bdc670d2522bdf8207b77accf22bbb0cc58343f3
vedakshari004 <vedakshari2999@gmail.com>
2025-06-27 19:09:11 +0530

    commiting the project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public int sampleAdd(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Main!");
+    }
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public String greet(String name) {
+        return "Hello, " + name;
+    }
+
+    public Object getNull() {
+        return null;
+    }
+
+}
